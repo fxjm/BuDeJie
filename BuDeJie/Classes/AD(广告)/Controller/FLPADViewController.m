c83a3d48fb7dac5b1f00078cc04ded0f3e2a1d74
lipeng.feng <499171803@qq.com>
2019-08-24 11:11:49 +0800

    添加cocodpad

---
+++ BuDeJie/Classes/AD(广告)/Controller/FLPADViewController.m
@@ -0,0 +1,25 @@
+//
+//  FLPADViewController.m
+//  BuDeJie
+//
+//  Created by lipeng feng on 2019/8/24.
+//  Copyright © 2019 lipeng feng. All rights reserved.
+//
+
+#import "FLPADViewController.h"
+#import <AFNetworking/AFNetworking.h>
+@interface FLPADViewController ()
+
+@end
+
+@implementation FLPADViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // 6p:LaunchImage-800-Portrait-736h@3x.png
+    // 6:LaunchImage-800-667h@2x.png
+    // 5:LaunchImage-568h@2x.png
+    // 4s:LaunchImage@2x.png
+}
+@end
